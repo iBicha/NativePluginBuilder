0cbe0562d71ad9a71c34946dbbea61e7c820a106
Brahim Hadriche <brahimh@3dprintwestern.com>
2017-10-16 16:37:23 -0600

    Get cmake version

---
+++ Assets/UnityNativePluginBuilder/Editor/CMake.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class CMake {
+    public static string GetCMakeVersion()
+    {
+        Process cmake = new Process();
+        cmake.StartInfo.FileName = "cmake";
+        cmake.StartInfo.Arguments = "--version";
+        cmake.StartInfo.UseShellExecute = false;
+        cmake.StartInfo.CreateNoWindow = true;
+        cmake.StartInfo.RedirectStandardOutput = true;
+        cmake.StartInfo.RedirectStandardError = true;
+
+        cmake.Start();
+
+        string output = cmake.StandardOutput.ReadToEnd();
+        string err = cmake.StandardError.ReadToEnd();
+        cmake.WaitForExit();
+
+        if(cmake.ExitCode != 0)
+        {
+            throw new System.Exception(string.Format("CMake exited with code {0} and error: {1}", cmake.ExitCode, err));
+        }
+
+        output = output.ToLower();
+
+        if (output.Contains("version"))
+        {
+            output = output.Substring(output.IndexOf("version") + "version".Length).Trim().Split(' ')[0];
+        }
+
+        return output;
+    }
+
+}
