44359f468678126552a5d0ad23bcbf6aa9323fe1
Brahim Hadriche <brahimh@3dprintwestern.com>
2017-10-16 16:21:00 -0600

    plugin editor window

---
+++ Assets/UnityNativePluginBuilder/Editor/NativePluginSettings.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+namespace iBicha
+{
+    [System.Serializable]
+    public class NativePluginSettings : ScriptableObject
+    {
+        private const string settingsFile = "Assets/UnityNativePluginBuilder/Editor/Resources/NativePluginSettings.asset";
+
+        public List<NativePlugin> plugins = new List<NativePlugin>();
+
+        private static NativePluginSettings get;
+        public static NativePluginSettings Get
+        {
+            get
+            {
+                if (get == null)
+                {
+                    Load();
+                }
+                return get;
+            }
+        }
+
+        public static void Load(string fromFile = "")
+        {
+            if (string.IsNullOrEmpty(fromFile))
+            {
+                fromFile = settingsFile;
+            }
+            UnityEngine.Object[] array = UnityEditorInternal.InternalEditorUtility.LoadSerializedFileAndForget(fromFile);
+            if (array.Length > 0 && array[0] != null)
+            {
+                get = array[0] as NativePluginSettings;
+            }
+            if (get == null)
+            {
+                get = ScriptableObject.CreateInstance<NativePluginSettings>();
+            }
+        }
+
+        public static void Save(string toFile = "")
+        {
+            if (string.IsNullOrEmpty(toFile))
+            {
+                toFile = settingsFile;
+            }
+            UnityEditorInternal.InternalEditorUtility.SaveToSerializedFileAndForget(new UnityEngine.Object[] { Get }, toFile, false);
+            AssetDatabase.Refresh();
+        }
+
+        [System.Serializable]
+        public class NativePlugin
+        {
+            public static NativePlugin GetDefault()
+            {
+                NativePlugin plugin = new NativePlugin();
+                plugin.Name = "MyPlugin";
+                plugin.Version = "1.0.0.0";
+                return plugin;
+            }
+
+            public string Name;
+            public string Version;
+
+            public string pluginBinaryFolder;
+            public string sourceFolder;
+
+            public void Build()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+
+}
---
+++ Assets/UnityNativePluginBuilder/Editor/UnityNativePluginBuilder.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+using UnityEngine;
+using UnityEngine.Events;
+namespace iBicha
+{
+    public class UnityNativePluginBuilder : EditorWindow
+    {
+        private bool createNewPlugin = false;
+        private NativePluginSettings.NativePlugin newPlugin = NativePluginSettings.NativePlugin.GetDefault();
+
+        private static GUIStyle _categoryBox;
+        private static GUIStyle categoryBox
+        {
+            get
+            {
+                if (_categoryBox == null)
+                {
+                    _categoryBox = new GUIStyle(GetStyle("HelpBox"));
+                    _categoryBox.padding.left = 14;
+                }
+                return _categoryBox;
+            }
+        }
+        private static int selectedSection = -1;
+        private static AnimBool[] SectionAnimators;
+
+        [MenuItem("Window/Unity Native Plugin Builder")]
+        static void Init()
+        {
+            // Get existing open window or if none, make a new one:
+            UnityNativePluginBuilder window = (UnityNativePluginBuilder)EditorWindow.GetWindow(typeof(UnityNativePluginBuilder));
+            window.titleContent.text = "Unity Native Plugin Builder";
+            window.Show();
+        }
+
+        private void OnEnable()
+        {
+            NativePluginSettings.Load();
+            ResizeSectionAnimators();
+        }
+
+        private void OnDisable()
+        {
+            NativePluginSettings.Save();
+        }
+
+
+        void ResizeSectionAnimators()
+        {
+            SectionAnimators = new AnimBool[NativePluginSettings.Get.plugins.Count];
+
+            for (int j = 0; j < SectionAnimators.Length; j++)
+            {
+                SectionAnimators[j] = new AnimBool(selectedSection == j, new UnityAction(base.Repaint));
+            }
+
+        }
+
+        void OnGUI()
+        {
+            OnGuiPlugins();
+
+            OnGuiNewPlugin();
+
+        }
+
+        void OnGuiPlugins()
+        {
+            EditorGUILayout.LabelField("Plugins", EditorStyles.boldLabel);
+
+            EditorGUILayout.Space();
+
+            for (int i = 0; i < NativePluginSettings.Get.plugins.Count; i++)
+            {
+                if (BeginSettingsBox(i, new GUIContent(NativePluginSettings.Get.plugins[i].Name)))
+                {
+                    NativePluginSettings.NativePlugin plugin = NativePluginSettings.Get.plugins[i];
+                    OnGuiNativePlugin(plugin);
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("Build", GUILayout.Width(110)))
+                    {
+                        plugin.Build();
+                    }
+                    if (GUILayout.Button("Remove", GUILayout.Width(110)))
+                    {
+                        if (EditorUtility.DisplayDialog("Remove " + plugin.Name + "?", "This will remove the plugin from the builder. Source files will not be deleted.", "Remove", "Cancel"))
+                        {
+                            NativePluginSettings.Get.plugins.Remove(plugin);
+                            i--;
+                        }
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+                EndSettingsBox();
+            }
+
+            EditorGUILayout.Space();
+
+        }
+
+        void OnGuiNewPlugin()
+        {
+            EditorGUILayout.LabelField("Create plugin", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+            if (!createNewPlugin)
+            {
+                if (GUILayout.Button("Create new plugin"))
+                {
+                    createNewPlugin = true;
+                }
+            }
+            else
+            {
+                OnGuiNativePlugin(newPlugin);
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button("Create"))
+                {
+                    NativePluginSettings.Get.plugins.Add(newPlugin);
+                    newPlugin = NativePluginSettings.NativePlugin.GetDefault();
+                    ResizeSectionAnimators();
+                    createNewPlugin = false;
+                }
+
+                if (GUILayout.Button("Cancel"))
+                {
+                    createNewPlugin = false;
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.Space();
+
+        }
+
+
+        void OnGuiNativePlugin(NativePluginSettings.NativePlugin plugin)
+        {
+            plugin.Name = EditorGUILayout.TextField("Plugin name", plugin.Name);
+            plugin.Version = EditorGUILayout.TextField("Version", plugin.Version);
+            EditorGUILayout.BeginHorizontal();
+            plugin.sourceFolder = EditorGUILayout.TextField("Source Folder", plugin.sourceFolder);
+            if (GUILayout.Button("Browse...", GUILayout.Width(90)))
+            {
+                string folder = EditorUtility.OpenFolderPanel("Select Source Folder", "", "");
+                if (!string.IsNullOrEmpty(folder) && System.IO.Directory.Exists(folder))
+                {
+                    plugin.sourceFolder = folder;
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+            plugin.pluginBinaryFolder = EditorGUILayout.TextField("Plugin Folder", plugin.pluginBinaryFolder);
+        }
+
+        private bool BeginSettingsBox(int nr, GUIContent header)
+        {
+            GUI.changed = false;
+            bool enabled = GUI.enabled;
+            GUI.enabled = true;
+            EditorGUILayout.BeginVertical(categoryBox, new GUILayoutOption[0]);
+            Rect rect = GUILayoutUtility.GetRect(20f, 18f);
+            rect.x += 3f;
+            rect.width += 6f;
+            bool flag = GUI.Toggle(rect, selectedSection == nr, header, GetStyle("IN TitleText"));
+            if (GUI.changed)
+            {
+                selectedSection = ((!flag) ? -1 : nr);
+            }
+            SectionAnimators[nr].target = flag;
+            GUI.enabled = enabled;
+            return EditorGUILayout.BeginFadeGroup(SectionAnimators[nr].faded);
+        }
+        private void EndSettingsBox()
+        {
+            EditorGUILayout.EndFadeGroup();
+            EditorGUILayout.EndVertical();
+        }
+        private static GUIStyle GetStyle(string styleName)
+        {
+            GUIStyle gUIStyle = GUI.skin.FindStyle(styleName);
+            if (gUIStyle == null)
+            {
+                gUIStyle = EditorGUIUtility.GetBuiltinSkin(EditorSkin.Inspector).FindStyle(styleName);
+            }
+            if (gUIStyle == null)
+            {
+                Debug.LogError("Missing built-in guistyle " + styleName);
+            }
+            return gUIStyle;
+        }
+    }
+
+}
