9ea7e1cc9d1ab815d0606fb410ac0bced9768c42
Brahim Hadriche <brahimh@3dprintwestern.com>
2017-10-18 15:41:16 -0600

    My miserable attempt at cmake

--- Assets/NativePluginBuilder/Editor/CMakeHelper.cs
+++ Assets/NativePluginBuilder/Editor/CMakeHelper.cs
@@ -14,7 +14,7 @@ namespace iBicha
 			cmake.StartInfo.FileName = FindBinary("cmake");
             cmake.StartInfo.Arguments = "--version";
             cmake.StartInfo.UseShellExecute = false;
-            cmake.StartInfo.CreateNoWindow = true;
+            //cmake.StartInfo.CreateNoWindow = true;
             cmake.StartInfo.RedirectStandardOutput = true;
             cmake.StartInfo.RedirectStandardError = true;
 
@@ -40,20 +40,28 @@ namespace iBicha
         }
 
 
-        public static bool Build(NativePlugin plugin)
-        { 
+        public static bool Build(NativePlugin plugin, bool release = true, string arch=null)
+        {
+            //cmake --build . --clean-first
             StringBuilder argsBuilder = new StringBuilder();
             argsBuilder.AppendFormat("{0} ", "../CMake");
             argsBuilder.AppendFormat("-DPLUGIN_NAME:STRING={0} ", plugin.Name);
             argsBuilder.AppendFormat("-DSOURCE_FOLDER:PATH={0} ", plugin.sourceFolder);
             argsBuilder.AppendFormat("-DPLUGIN_BINARY_FOLDER:PATH={0} ", plugin.pluginBinaryFolderPath);
+            argsBuilder.AppendFormat("-DPLUGIN_BINARY_FOLDER:PATH={0} ", plugin.pluginBinaryFolderPath);
+            argsBuilder.AppendFormat("-DCMAKE_BUILD_TYPE={0} ", release ? "Debug" : "Release");
+            if(!string.IsNullOrEmpty(arch))
+            {
+                argsBuilder.AppendFormat("-DARCH={0} ", arch);
+            }
+            //argsBuilder.AppendFormat("--build {0}", "../CMake");
 
             Process cmake = new Process();
 			cmake.StartInfo.FileName = FindBinary("cmake");
             cmake.StartInfo.Arguments = argsBuilder.ToString();
             cmake.StartInfo.WorkingDirectory = plugin.buildFolder;
             cmake.StartInfo.UseShellExecute = false;
-            cmake.StartInfo.CreateNoWindow = true;
+            //cmake.StartInfo.CreateNoWindow = true;
             cmake.StartInfo.RedirectStandardOutput = true;
             cmake.StartInfo.RedirectStandardError = true;
 
@@ -67,10 +75,11 @@ namespace iBicha
                 return false;
                 //throw ex;
             }
-
+            string output = "";
             while (!cmake.StandardOutput.EndOfStream)
             {
                 NativePluginBuilder.lastLogLine = cmake.StandardOutput.ReadLine();
+                output += NativePluginBuilder.lastLogLine + System.Environment.NewLine;
             }
 
             string err = cmake.StandardError.ReadToEnd();
@@ -78,6 +87,8 @@ namespace iBicha
             {
                 UnityEngine.Debug.LogError(err);
             }
+            UnityEngine.Debug.Log(output);
+
             cmake.WaitForExit();
 
             return cmake.ExitCode == 0;
--- Assets/NativePluginBuilder/Editor/NativePlugin.cs
+++ Assets/NativePluginBuilder/Editor/NativePlugin.cs
@@ -39,7 +39,7 @@ namespace iBicha
 
         public void Build()
         {
-            CMakeHelper.Build(this);
+            CMakeHelper.Build(this, true, "x86_64");
         }
     }
 }
