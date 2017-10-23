a632c4e1a5c59fb0920cd97539771f99eb3f4d05
Brahim Hadriche <brahim.hadriche@gmail.com>
2017-10-23 09:50:24 -0600

    Change iOS to static lib, working example (TODO: cleanup example)

--- Assets/Example/MyPluginExample.cs
+++ Assets/Example/MyPluginExample.cs
@@ -2,29 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+
 namespace iBicha.Example {
 	public class MyPluginExample : MonoBehaviour {
 
+
+
         public UnityEngine.UI.Text console;
+
+		private static UnityEngine.UI.Text s_console;
+
 		// Use this for initialization
 		void Start () {
+			s_console = console;
+
             Log(string.Format("GetTwo() returned:{0}", MyPlugin.GetTwo()));
 
-            Log(string.Format("PassCallback() returned:{0}", MyPlugin.PassCallback((result) => {
-                Log(string.Format("The callback result is:{0}", result));
-            })));
+			Log(string.Format("PassCallback() returned:{0}", MyPlugin.PassCallback(MyPlugin.Callback)));
 
             int[] array = { 0, 0, 0, 0, 0 };
             MyPlugin.FillWithOnes(array, array.Length);
             Log(string.Format("The content of array is:{0}", "[" + string.Join(",", array.Select(i => i.ToString()).ToArray()) + "]"));
         }
-
-        private void Log(string obj)
+			
+		public static void Log(string obj)
         {
             Debug.Log(obj);
-            if (console != null)
+			if (s_console != null)
             {
-                console.text += obj + System.Environment.NewLine;
+				s_console.text += obj + System.Environment.NewLine;
             }
 
         }
--- Assets/MyPlugin/MyPlugin.cs
+++ Assets/MyPlugin/MyPlugin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+using AOT;
 
 
 namespace iBicha.Example
@@ -28,6 +29,12 @@ namespace iBicha.Example
         //Pass an array to the c++ code, and fill it with 1s.
         [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void FillWithOnes(int[] array, int length);
+
+
+		[MonoPInvokeCallback(typeof(CallbackDelegate))]
+		public static void Callback(int result) {
+			MyPluginExample.Log(string.Format("The callback result is:{0}", result));
+		}
 
     }
 }
