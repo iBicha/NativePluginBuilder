c21ea687125452bdfe0df82309b3894b49e5f4e4
Brahim Hadriche <brahimh@3dprintwestern.com>
2017-10-31 15:58:53 -0600

    Hide processes if too many for status bar

--- Assets/NativePluginBuilder/Editor/BackgroundProcessManager.cs
+++ Assets/NativePluginBuilder/Editor/BackgroundProcessManager.cs
@@ -7,6 +7,8 @@ namespace iBicha
     public class BackgroundProcessManager
     {
 
+        private bool expandGui;
+
         public static List<BackgroundProcess> BackgroundProcesses = new List<BackgroundProcess>();
 
         public static void Add(BackgroundProcess process)
@@ -55,22 +57,49 @@ namespace iBicha
                 EditorGUILayout.BeginVertical(NativePluginBuilder.categoryBox, new GUILayoutOption[0]);
                 GUILayout.Label("Idle.");
                 EditorGUILayout.EndVertical();
+                return;
             }
-            else
+
+            if (!expandGui && BackgroundProcesses.Count > 2)
             {
-                for (int i = 0; i < BackgroundProcesses.Count; i++)
+                EditorGUILayout.BeginVertical(NativePluginBuilder.categoryBox, new GUILayoutOption[0]);
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Label(string.Format("{0} Processes running in the background", BackgroundProcesses.Count), EditorStyles.boldLabel);
+                if (GUILayout.Button("Show", GUILayout.Width(80)))
                 {
-                    EditorGUILayout.BeginVertical(NativePluginBuilder.categoryBox, new GUILayoutOption[0]);
-                    EditorGUILayout.BeginHorizontal();
-                    GUILayout.Label(BackgroundProcesses[i].Name, EditorStyles.boldLabel);
-                    if (GUILayout.Button("Stop", GUILayout.Width(60)))
-                    {
-                        BackgroundProcesses[i].Stop();
-                    }
-                    EditorGUILayout.EndHorizontal();
-                    GUILayout.Label(BackgroundProcesses[i].lastLine, EditorStyles.miniLabel);
-                    EditorGUILayout.EndVertical();
+                    expandGui = true;
                 }
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.EndVertical();
+
+                return;
+            }
+
+            if (BackgroundProcesses.Count > 2)
+            {
+                EditorGUILayout.BeginVertical(NativePluginBuilder.categoryBox, new GUILayoutOption[0]);
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Label(string.Format("{0} Processes running in the background", BackgroundProcesses.Count), EditorStyles.boldLabel);
+                if (GUILayout.Button("Hide", GUILayout.Width(80)))
+                {
+                    expandGui = false;
+                }
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.EndVertical();
+            }
+
+            for (int i = 0; i < BackgroundProcesses.Count; i++)
+            {
+                EditorGUILayout.BeginVertical(NativePluginBuilder.categoryBox, new GUILayoutOption[0]);
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Label(BackgroundProcesses[i].Name, EditorStyles.boldLabel);
+                if (GUILayout.Button("Stop", GUILayout.Width(60)))
+                {
+                    BackgroundProcesses[i].Stop();
+                }
+                EditorGUILayout.EndHorizontal();
+                GUILayout.Label(BackgroundProcesses[i].lastLine, EditorStyles.miniLabel);
+                EditorGUILayout.EndVertical();
             }
         }
     }
